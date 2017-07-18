061a0b6d1a2e8c02eb0bb38c4feff0d32ef68c90
andrewomacdonald <andrew.o.macdonald@gmail.com>
2017-07-18 11:46:50 -0700

    moved package.json, server, and json file out of public folder

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.get('/products', function(request, response) {
     response.json(salesData);
 });
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules/'))
 
 var server=app.listen(app.get('port'), function() {
     console.log("We have started our server at", app.get('port'));
